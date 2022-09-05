1b9737c9780c4b55a7e96f6eb77a4007b267452a
Levi R <levir620@skiff.com>
2022-09-05 16:38:44 -0700

    ✨ frontpage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,50 @@
 <script lang="ts">
 	import Logo from '../components/logo.svelte';
+	import '@fontsource/red-hat-display/700.css';
 </script>
 
 <main>
-	<Logo />
+	<div class="hero">
+		<div class="row center">
+			<span
+				on:contextmenu={(event) => {
+					event.preventDefault();
+				}}><Logo dimensions={128} /></span
+			>
+			<span class="column">
+				<h1 class="hero-text-big">The Video Platform for Humans.</h1>
+			</span>
+		</div>
+		<p>
+			Huelet is the free and open-source video platform for everyone. Sign up and immediately start
+			posting videos, gain an audience, and grow your community!
+		</p>
+		<div class="button primary">Join Our Community</div>
+		<div class="button secondary">I already have an account</div>
+	</div>
 </main>
 
-<style></style>
+<style>
+	main {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+		width: 100vw;
+	}
+	.hero {
+		padding: 1rem;
+	}
+
+	.hero-text {
+		font-size: 2rem;
+		font-weight: 700;
+		margin-left: 1rem;
+	}
+
+	@media (min-width: 768px) {
+		.hero {
+			padding: 2rem;
+		}
+	}
+</style>
