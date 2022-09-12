c13f54bae4ba9b589c41d567e2c00c0475d8dcbe
Levi R <levir620@skiff.com>
2022-09-11 19:17:48 -0700

    ✨ video player

--- src/routes/w/[vuid]/+page.svelte
+++ src/routes/w/[vuid]/+page.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+	import Player from '../../../components/player.svelte';
 	import Header from '../../../components/header.svelte';
 
 	export let vuid: string;
-	export let video: object;
+	export let video: any;
 	export let error: string;
 
 	vuid = $page.params.vuid;
@@ -24,4 +25,22 @@
 </script>
 
 <Header />
-<div>{$page.params.vuid}</div>
+<div>
+	<Player
+		thumbnail={typeof video !== 'undefined' ? video.thumbnail : null}
+		sources={{
+			hd: {
+				mp4_h264:
+					'https://videos.cdn.huelet.net/videoasset-10n3ip7xyvj53eyn0-1647063696/z5elsaa31373ujv0w1455yr68jwaws8ub4.mov?sv=2020-02-10&ss=b&srt=sco&sp=r&se=3000-06-27T09:40:10Z&st=2021-06-27T01:40:10Z&sip=0.0.0.0-255.255.255.255&spr=https&sig=LDOCpb7z9CSWk2GkFNlalqVWOhdwmwn2pSBWbSBnVtM%3D',
+				webm_h264:
+					'https://videos.cdn.huelet.net/videoasset-10n3ip7xyvj53eyn0-1647063696/z5elsaa31373ujv0w1455yr68jwaws8ub4.mov?sv=2020-02-10&ss=b&srt=sco&sp=r&se=3000-06-27T09:40:10Z&st=2021-06-27T01:40:10Z&sip=0.0.0.0-255.255.255.255&spr=https&sig=LDOCpb7z9CSWk2GkFNlalqVWOhdwmwn2pSBWbSBnVtM%3D'
+			},
+			sd: {
+				mp4_h264:
+					'https://videos.cdn.huelet.net/videoasset-10n3ip7xyvj53eyn0-1647063696/z5elsaa31373ujv0w1455yr68jwaws8ub4.mov?sv=2020-02-10&ss=b&srt=sco&sp=r&se=3000-06-27T09:40:10Z&st=2021-06-27T01:40:10Z&sip=0.0.0.0-255.255.255.255&spr=https&sig=LDOCpb7z9CSWk2GkFNlalqVWOhdwmwn2pSBWbSBnVtM%3D',
+				webm_h264:
+					'https://videos.cdn.huelet.net/videoasset-10n3ip7xyvj53eyn0-1647063696/z5elsaa31373ujv0w1455yr68jwaws8ub4.mov?sv=2020-02-10&ss=b&srt=sco&sp=r&se=3000-06-27T09:40:10Z&st=2021-06-27T01:40:10Z&sip=0.0.0.0-255.255.255.255&spr=https&sig=LDOCpb7z9CSWk2GkFNlalqVWOhdwmwn2pSBWbSBnVtM%3D'
+			}
+		}}
+	/>
+</div>
