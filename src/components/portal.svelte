0b3a74183d723f553796ea9cbd9ea4efb5a37d9b
Levi R <levir620@skiff.com>
2022-09-17 13:20:28 -0700

    🎨 portal component

---
+++ src/components/portal.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import { onMount, onDestroy } from 'svelte';
+
+	let ref: HTMLElement;
+	let portal: HTMLElement;
+
+	onMount(() => {
+		if (typeof document !== 'undefined') {
+			portal = document.createElement('div');
+			portal.className = 'portal';
+			document.body.appendChild(portal);
+			portal.appendChild(ref);
+		}
+	});
+
+	onDestroy(() => {
+		if (typeof document !== 'undefined') {
+			document.body.removeChild(portal);
+		}
+	});
+</script>
+
+<div class="portal-clone">
+	<div bind:this={ref}>
+		<slot />
+	</div>
+</div>
+
+<style>
+	.portal-clone {
+		display: none;
+	}
+</style>
