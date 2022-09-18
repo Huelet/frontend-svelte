1ac5990e4a3a7c26e684b8051f005b787df7f574
Levi R <levir620@skiff.com>
2022-09-18 14:32:05 -0700

    🧑‍💻 layout for auth routes

---
+++ src/routes/auth/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Header from '../../components/header.svelte';
+</script>
+
+<main>
+	<Header />
+	<slot />
+</main>
+
+<style>
+	main {
+		height: 100vh;
+		width: 100vw;
+	}
+</style>
