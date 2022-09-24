396d8981aa1d7e3fe57c42895e272f56cb248423
Levi R <levir620@skiff.com>
2022-09-24 13:38:14 -0700

    🧑‍💻 avatar component

---
+++ src/components/avatar.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+	export let username: string = '';
+	export let userID: string = '';
+	export let url: string = '';
+	export let dimensions: number = 64;
+	export let forceAltText: string = '';
+	export let styles: string = '';
+
+	if (url === '') {
+		if (userID === '') {
+			fetch(`https://api.huelet.net/auth/pfp?username=${username}`)
+				.then((response) => response.json())
+				.then((data) => {
+					url = data.pfp;
+				});
+		} else if (username === '') {
+			fetch(`https://api.huelet.net/auth/pfp?uid=${userID}`)
+				.then((response) => response.json())
+				.then((data) => {
+					url = data.pfp;
+				});
+		}
+	}
+
+	if (dimensions !== 64)
+		styles = `width: ${dimensions}px !important; height: ${dimensions}px !important;`;
+	else styles = `width: ${dimensions}px !important; height: ${dimensions}px !important;`;
+
+	styles += `box-sizing: border-box; display: inline-block; overflow: hidden; width: initial; height: initial; background: none; opacity: 1; border: 0px; margin: 0px; padding: 0px; position: relative; max-width: 100%;`;
+</script>
+
+<span
+	style="box-sizing: border-box; display: inline-block; overflow: hidden; width: initial; height: initial; background: none; opacity: 1; border: 0px; margin: 0px; padding: 0px; position: relative; max-width: 100%;"
+>
+	<span
+		style="box-sizing: border-box; display: inline-block; overflow: hidden; width: initial; height: initial; background: none; opacity: 1; border: 0px; margin: 0px; padding: 0px; position: relative; max-width: 100%;"
+	>
+		<img
+			src={url}
+			alt={forceAltText === '' ? `Profile picture` : forceAltText}
+			width={dimensions}
+			height={dimensions}
+			style={styles}
+		/>
+	</span>
+</span>
+
+<style>
+	img {
+		padding: 2px !important;
+		border-radius: 50% !important;
+		border: 2px solid #7600ff !important;
+		image-rendering: optimizeSpeed;
+	}
+</style>
