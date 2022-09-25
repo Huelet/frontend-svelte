9557970d0f05ff62e9bc018bc97110cb832a298d
Levi R <levir620@skiff.com>
2022-09-25 16:44:58 -0700

    🧑‍💻 add chip component

---
+++ src/components/chip.svelte
@@ -0,0 +1,24 @@
+<div class="chip">
+	<slot />
+</div>
+
+<style>
+	.chip {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+
+		padding: 0.5em 1em;
+		border-radius: 5rem;
+
+        background: var(--accent-primary-dark);
+
+		cursor: pointer;
+		box-sizing: border-box;
+		user-select: none;
+
+		width: 9em;
+		height: 1em;
+	}
+</style>
