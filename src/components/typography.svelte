d44963d24ee52b8c50fc73949bac4ad921a35ae1
Levi <levir620@skiff.com>
2022-10-10 23:15:20 +0000

    🧑‍💻 add typography component

---
+++ src/components/typography.svelte
@@ -0,0 +1,97 @@
+<script lang="ts">
+	import type { Size, FontPreset } from '../types/components';
+
+	export let preset: FontPreset | undefined = undefined;
+	export let weight: number | undefined = undefined;
+	export let size: Size | undefined = undefined;
+	export let color: string | undefined = undefined;
+</script>
+
+{#if !preset}
+	<span class="typography-reset-global {size === 'lg' || 'xl' ? 'typography-use-lg' : 'typography-use-sm'}">
+		<p
+			class="
+            text chonky-{weight || '400'} chonky-{size || 'md'}
+        "
+			style="
+            color: {color || '#ffffff'};
+        "
+		>
+			<slot />
+		</p>
+	</span>
+{/if}
+
+<style>
+	.typography-reset-global {
+		margin: 0;
+		padding: 0;
+		border: 0;
+	}
+
+    .typography-use-sm {
+        font-family: 'Red Hat Text', sans-serif;
+    }
+
+    .typography-use-lg {
+        font-family: 'Red Hat Display', sans-serif;
+    }
+
+	.text {
+		margin: 0;
+		padding: 0;
+		border: 0;
+	}
+    
+    .chonky-100 {
+        font-weight: 100;
+    }
+
+    .chonky-200 {
+        font-weight: 200;
+    }
+
+    .chonky-300 {
+        font-weight: 300;
+    }
+
+    .chonky-400 {
+        font-weight: 400;
+    }
+
+    .chonky-500 {
+        font-weight: 500;
+    }
+
+    .chonky-600 {
+        font-weight: 600;
+    }
+
+    .chonky-700 {
+        font-weight: 700;
+    }
+
+    .chonky-800 {
+        font-weight: 800;
+    }
+
+    .chonky-xs {
+        font-size: 0.75em;
+    }
+
+    .chonky-sm {
+        font-size: 0.875em;
+    }
+
+    .chonky-md {
+        font-size: 1em;
+    }
+
+    .chonky-lg {
+        font-size: 1.25em;
+    }
+
+    .chonky-xl {
+        font-size: 1.5em;
+    }
+</style>
---
+++ src/types/components.ts
@@ -0,0 +1,3 @@
+export type FontPreset = "Heading" | "Subheading" | "Body" | "Bold" | "Caption" | "Italic";
+export type Size = "xs" | "sm" | "md" | "lg" | "xl";
+export type FontWeight = "black" | "normal" | "bold" | "bolder" | "lighter"
