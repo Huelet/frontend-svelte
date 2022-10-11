1f6b88c9b129bbbae81c34ddb6f91f1be2ef44c7
Levi <levir620@skiff.com>
2022-10-11 18:13:56 +0000

    💄 page layout for safety/security and search

---
+++ src/routes/s/+layout.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import Header from '../../components/header.svelte';
+</script>
+
+<main>
+	<Header />
+	<div class="page-content">
+		<slot />
+	</div>
+</main>
+
+<style>
+	main {
+		height: 100vh;
+		width: 100vw;
+	}
+
+	.page-content {
+		padding: 32px 0 0 0;
+	}
+</style>
