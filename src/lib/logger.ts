bdda6aad539f6c98b727309c6f0011ce96090ad8
Levi <levir620@skiff.com>
2022-10-22 01:35:53 +0000

    🧑‍💻 basic logging service

---
+++ src/lib/logger.ts
@@ -0,0 +1,15 @@
+export class Logger {
+	constructor(private readonly name: string) {}
+
+	neutral(message: string) {
+		console.log(`[${this.name} at ${Date.now()}] INFO: ${message}`);
+	}
+
+    warning(message: string) {
+        console.warn(`[${this.name} at ${Date.now()}] WARNING: ${message}`);
+    }
+
+    error(message: string) {
+        console.error(`[${this.name} at ${Date.now()}] ERROR: ${message}`);
+    }
+}
