b4811da8718d0f4b29efe2772ff1a5390c0ca0f1
Levi <levir620@skiff.com>
2022-10-22 01:37:04 +0000

    🧑‍💻 add root component

---
+++ src/components/root.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import { Logger } from '../lib/logger';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		const logger = new Logger('root element');
+
+		logger.neutral('component mounted');
+
+		class RootElement extends HTMLElement {
+			constructor() {
+				super();
+			}
+
+			connectedCallback() {
+				this.classList.add('root');
+			}
+		}
+
+		customElements.define('root', RootElement);
+	});
+</script>
+
+<root class="root">
+	<slot />
+</root>
+
+<style>
+	:host {
+		display: block;
+	}
+
+	.root {
+		width: 100vw;
+		height: 100vh;
+	}
+</style>
