edec910548e3cb12f3d3106e6e346b1a01431b3d
Levi <levir620@skiff.com>
2022-10-23 03:00:52 +0000

    🚨 run prettier

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -5,11 +5,11 @@ export class Logger {
 		console.log(`[${this.name} at ${Date.now()}] INFO: ${message}`);
 	}
 
-    warning(message: string) {
-        console.warn(`[${this.name} at ${Date.now()}] WARNING: ${message}`);
-    }
+	warning(message: string) {
+		console.warn(`[${this.name} at ${Date.now()}] WARNING: ${message}`);
+	}
 
-    error(message: string) {
-        console.error(`[${this.name} at ${Date.now()}] ERROR: ${message}`);
-    }
+	error(message: string) {
+		console.error(`[${this.name} at ${Date.now()}] ERROR: ${message}`);
+	}
 }
--- src/types/components.ts
+++ src/types/components.ts
@@ -1,3 +1,3 @@
-export type FontPreset = "Heading" | "Subheading" | "Body" | "Bold" | "Caption" | "Italic";
-export type Size = "xs" | "sm" | "md" | "lg" | "xl";
-export type FontWeight = "black" | "normal" | "bold" | "bolder" | "lighter"
+export type FontPreset = 'Heading' | 'Subheading' | 'Body' | 'Bold' | 'Caption' | 'Italic';
+export type Size = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+export type FontWeight = 'black' | 'normal' | 'bold' | 'bolder' | 'lighter';
