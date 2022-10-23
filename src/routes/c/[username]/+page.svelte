86283e0546b3b0ea8ebd3919adb2f04e3fa3a0f3
Levi <levir620@skiff.com>
2022-10-23 03:00:06 +0000

    🎨 use icon components

--- src/routes/c/[username]/+page.svelte
+++ src/routes/c/[username]/+page.svelte
@@ -8,6 +8,7 @@
 	import Follow from '../../../components/buttons/follow.svelte';
 	import Root from '../../../components/root.svelte';
 	import VideoCard from '../../../components/video-card.svelte';
+	import { Notepad, Avatar as AvatarIcon, Calendar, Location } from '../../../components/icons';
 
 	let user: any;
 	let username: string = $page.params.username.replace('@', '');
@@ -60,28 +61,7 @@
 						{#if loading}
 							<Skeleton width={100} height={15} />
 						{:else}
-							<svg
-								width="16"
-								height="16"
-								viewBox="0 0 16 16"
-								xmlns="http://www.w3.org/2000/svg"
-								fill="white"
-							>
-								<path
-									fill-rule="evenodd"
-									clip-rule="evenodd"
-									d="M5 1C5 0.447715 4.55229 0 4 0C3.44771 0 3 0.447715 3 1V2.17071C1.83481 2.58254 1 3.69378 1 5V13C1 14.6569 2.34315 16 4 16H12C13.6569 16 15 14.6569 15 13V5C15 3.69378 14.1652 2.58254 13 2.17071V1C13 0.447715 12.5523 0 12 0C11.4477 0 11 0.447715 11 1V2H9V1C9 0.447715 8.55228 0 8 0C7.44772 0 7 0.447715 7 1V2H5V1ZM12 4C12.5523 4 13 4.44772 13 5V13C13 13.5523 12.5523 14 12 14H4C3.44772 14 3 13.5523 3 13V5C3 4.44772 3.44772 4 4 4H12Z"
-								/>
-								<path
-									d="M5 5C4.44772 5 4 5.44772 4 6C4 6.55228 4.44772 7 5 7H11C11.5523 7 12 6.55228 12 6C12 5.44772 11.5523 5 11 5H5Z"
-								/>
-								<path
-									d="M5 8C4.44772 8 4 8.44772 4 9C4 9.55228 4.44772 10 5 10H9C9.55228 10 10 9.55228 10 9C10 8.44772 9.55228 8 9 8H5Z"
-								/>
-								<path
-									d="M5 11C4.44772 11 4 11.4477 4 12C4 12.5523 4.44772 13 5 13H7C7.55228 13 8 12.5523 8 12C8 11.4477 7.55228 11 7 11H5Z"
-								/>
-							</svg>
+							<Notepad fill="white" />
 							<Typography size={'sm'}
 								><p class="bio">
 									{@html user?.bio}
@@ -90,57 +70,15 @@
 						{/if}
 					</span>
 					<span class="row">
-						<svg
-							width="16"
-							height="16"
-							viewBox="0 0 16 16"
-							xmlns="http://www.w3.org/2000/svg"
-							fill="white"
-						>
-							<path
-								d="M10 6C10 7.10457 9.10457 8 8 8C6.89543 8 6 7.10457 6 6C6 4.89543 6.89543 4 8 4C9.10457 4 10 4.89543 10 6Z"
-							/>
-							<path
-								fill-rule="evenodd"
-								clip-rule="evenodd"
-								d="M9.45618 15.8678C13.1792 15.1831 16 11.921 16 8C16 3.58172 12.4183 0 8 0C3.58172 0 0 3.58172 0 8C0 11.921 2.82082 15.1831 6.54382 15.8678C6.84774 15.9539 7.16849 16 7.5 16H8.5C8.83151 16 9.15226 15.9539 9.45618 15.8678ZM11.9999 12.4723C13.2275 11.3737 14 9.77705 14 8C14 4.68629 11.3137 2 8 2C4.68629 2 2 4.68629 2 8C2 9.77705 2.77255 11.3737 4.00011 12.4723C4.01499 10.5521 5.57624 9 7.5 9H8.5C10.4238 9 11.985 10.5521 11.9999 12.4723Z"
-							/>
-						</svg>
+						<AvatarIcon fill="white" />
 						<Typography size={'sm'}>{user?.pronouns.join('/')}</Typography>
 					</span>
 					<span class="row">
-						<svg
-							width="16"
-							height="16"
-							viewBox="0 0 16 16"
-							fill="white"
-							xmlns="http://www.w3.org/2000/svg"
-						>
-							<path
-								d="M8 8C9.10457 8 10 7.10457 10 6C10 4.89543 9.10457 4 8 4C6.89543 4 6 4.89543 6 6C6 7.10457 6.89543 8 8 8Z"
-							/>
-							<path
-								fill-rule="evenodd"
-								clip-rule="evenodd"
-								d="M14 6.36693C14 8.49125 11.8102 11.7281 10.0764 13.9579C8.99609 15.3474 7.00391 15.3474 5.92358 13.9579C4.1898 11.7281 2 8.49125 2 6.36693C2 2.85057 4.68629 0 8 0C11.3137 0 14 2.85057 14 6.36693ZM4 6.36693C4 3.84164 5.90105 2 8 2C10.099 2 12 3.84164 12 6.36693C12 6.87417 11.6894 7.8243 10.9468 9.13955C10.2506 10.3727 9.33544 11.6526 8.49753 12.7303C8.33733 12.9363 8.15257 13 8 13C7.84743 13 7.66267 12.9363 7.50247 12.7303C6.66456 11.6526 5.74942 10.3727 5.05321 9.13955C4.31062 7.8243 4 6.87417 4 6.36693Z"
-							/>
-						</svg>
+						<Location fill="white" />
 						<Typography size={'sm'}>{user?.location}</Typography>
 					</span>
 					<span class="row">
-						<svg
-							width="16"
-							height="16"
-							viewBox="0 0 16 16"
-							fill="white"
-							xmlns="http://www.w3.org/2000/svg"
-						>
-							<path
-								fill-rule="evenodd"
-								clip-rule="evenodd"
-								d="M6 0C5.44771 0 5 0.447715 5 1V2H4C2.34315 2 1 3.34315 1 5V13C1 14.6569 2.34315 16 4 16H12C13.6569 16 15 14.6569 15 13V5C15 3.34315 13.6569 2 12 2H11V1C11 0.447715 10.5523 0 10 0C9.44772 0 9 0.447715 9 1V2H7V1C7 0.447715 6.55229 0 6 0ZM12 4H11C11 4.55228 10.5523 5 10 5C9.44772 5 9 4.55228 9 4H7C7 4.55228 6.55229 5 6 5C5.44771 5 5 4.55228 5 4H4C3.44772 4 3 4.44772 3 5V6H13V5C13 4.44772 12.5523 4 12 4ZM13 8V13C13 13.5523 12.5523 14 12 14H4C3.44772 14 3 13.5523 3 13V8H13Z"
-							/>
-						</svg>
+						<Calendar fill="white" />
 						<Typography size={'sm'}>
 							Joined {typeof user === 'undefined'
 								? 'loading...'
--- src/routes/w/[vuid]/+page.svelte
+++ src/routes/w/[vuid]/+page.svelte
@@ -8,6 +8,7 @@
 	import Skeleton from '../../../components/skeleton.svelte';
 	import Follow from '../../../components/buttons/follow.svelte';
 	import Chip from '../../../components/chip.svelte';
+	import { WarningFilled, ChevronDown } from '../../../components/icons';
 
 	let description: boolean = false;
 	let vuid: string;
@@ -110,20 +111,10 @@
 						</div>
 						<div class="meta_item center">
 							<div class="description-toggle" on:click={() => (description = !description)}>
-								<svg
-									width="16"
-									height="16"
-									viewBox="0 0 16 16"
+								<ChevronDown
 									fill="white"
-									xmlns="http://www.w3.org/2000/svg"
 									class="description-toggle__icon {description ? 'description-toggle--open' : null}"
-								>
-									<path
-										fill-rule="evenodd"
-										clip-rule="evenodd"
-										d="M1.29289 4.29289C1.68342 3.90237 2.31658 3.90237 2.70711 4.29289L8 9.58579L13.2929 4.29289C13.6834 3.90237 14.3166 3.90237 14.7071 4.29289C15.0976 4.68342 15.0976 5.31658 14.7071 5.70711L8.70711 11.7071C8.31658 12.0976 7.68342 12.0976 7.29289 11.7071L1.29289 5.70711C0.902369 5.31658 0.902369 4.68342 1.29289 4.29289Z"
-									/>
-								</svg>
+								/>
 							</div>
 						</div>
 					{:else}
@@ -150,27 +141,7 @@
 		heading={error.title}
 		body={error.message}
 	>
-		<svg
-			width="16"
-			height="16"
-			viewBox="0 0 16 16"
-			fill="white"
-			xmlns="http://www.w3.org/2000/svg"
-			slot="icon"
-		>
-			<g clip-path="url(#clip0_1510_470)">
-				<path
-					fill-rule="evenodd"
-					clip-rule="evenodd"
-					d="M6.67433 1.50407L0.165688 13.7982C-0.363195 14.7972 0.361009 16 1.49137 16H14.5087C15.639 16 16.3632 14.7972 15.8343 13.7982L9.32569 1.50407C8.76228 0.439844 7.23774 0.439847 6.67433 1.50407ZM7.00001 6.00001C7.00001 5.44772 7.44773 5.00001 8.00001 5.00001C8.5523 5.00001 9.00001 5.44772 9.00001 6.00001V9.00001C9.00001 9.55229 8.5523 10 8.00001 10C7.44773 10 7.00001 9.55229 7.00001 9.00001V6.00001ZM7.00001 13C7.00001 12.4477 7.44773 12 8.00001 12C8.5523 12 9.00001 12.4477 9.00001 13C9.00001 13.5523 8.5523 14 8.00001 14C7.44773 14 7.00001 13.5523 7.00001 13Z"
-				/>
-			</g>
-			<defs>
-				<clipPath id="clip0_1510_470">
-					<rect width="16" height="16" fill="white" transform="matrix(-1 0 0 1 16 0)" />
-				</clipPath>
-			</defs>
-		</svg>
+		<WarningFilled fill="white" />
 	</Toast>
 {/if}
 
