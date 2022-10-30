e59e42e0be60f1e604bc6551737ec34104de2c94
Levi <levir620@skiff.com>
2022-10-30 04:08:02 +0000

    ⚡️ image optimization for logo component

--- src/components/logo.svelte
+++ src/components/logo.svelte
@@ -1,17 +1,13 @@
 <script lang="ts">
+	import logo from '$lib/static/logo.png';
+
 	export let dimensions = 64;
 
 	let className = '';
 	export { className as class };
 </script>
 
-<img
-	src="https://cdn.huelet.net/assets/logo.png"
-	alt="Huelet"
-	width={dimensions}
-	height={dimensions}
-	class={className}
-/>
+<img src={logo} alt="Huelet" width={dimensions} height={dimensions} class={className} />
 
 <style>
 	img {
