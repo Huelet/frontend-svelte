c023eb5b423f405207a301ea9b47603b32b58e92
Levi <levir620@skiff.com>
2022-11-10 23:41:41 +0000

    ✨ avatar and header viewer for profile

---
+++ src/routes/c/[username]/avatar/[number=int]/+page.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	import Avatar from '../../../../../components/avatar.svelte';
+	import Portal from '../../../../../components/portal.svelte';
+
+	let backgroundColor: string;
+
+	export let data: PageData;
+
+	$: backgroundColor = data.user.avatarColor;
+</script>
+
+<Portal class="portal full page center">
+	<div class="full page center avatar-viewer" style="background-color: {backgroundColor}e1">
+		<Avatar url={data.user.avatar} dimensions={256} />
+	</div>
+</Portal>
+
+<style>
+	:global(body, html) {
+		overflow: hidden;
+	}
+
+	.avatar-viewer {
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+	}
+
+	.avatar {
+		background-size: cover;
+		background-position: center;
+		background-repeat: no-repeat;
+	}
+</style>
---
+++ src/routes/c/[username]/header/[number=int]/+page.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	import { onMount } from 'svelte';
+	import Portal from '../../../../../components/portal.svelte';
+
+	let backgroundColor: string;
+	let styles: string;
+	let width: string;
+	let height: string;
+
+	onMount(() => {
+		width = window.innerWidth + 'px';
+		height = window.innerHeight / 2 + 'px';
+
+		styles = `width: ${width}; height: ${height}; background-image: url(${
+			data.user?.header || data.user?.avatar
+		});`;
+	});
+
+	export let data: PageData;
+
+	$: backgroundColor = data.user.avatarColor;
+</script>
+
+<Portal class="portal full page center">
+	<div class="full page center header-viewer" style="background-color: {backgroundColor}e1">
+		<div class="header" style={styles} />
+	</div>
+</Portal>
+
+<style>
+	:global(body, html) {
+		overflow: hidden;
+	}
+
+	.header-viewer {
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+    }
+
+	.header {
+		background-size: cover;
+		background-position: center;
+		background-repeat: no-repeat;
+	}
+</style>
