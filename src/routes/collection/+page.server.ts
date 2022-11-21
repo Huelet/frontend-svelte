c35134ce0ffb79290860a5c7398ee8a5c3c9ff1d
Levi <levir620@skiff.com>
2022-11-21 04:43:58 +0000

    ⚡️ remove random useless if statement

--- src/routes/collection/+page.server.ts
+++ src/routes/collection/+page.server.ts
@@ -1,8 +1,6 @@
 import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
- 
-export const load: PageServerLoad = ({ locals }: any) => {
-  if (!locals.user) {
-    throw redirect(308, '/collection/playlists/');
-  }
-}
+
+export const load: PageServerLoad = () => {
+	throw redirect(308, '/collection/playlists/');
+};
