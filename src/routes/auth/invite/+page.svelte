1601c0f5ec2745714717970e446d0a667bffaeca
Daniel Kravec <daniel@kravec.com>
2022-12-02 11:00:51 -0500

    there was a a lot of bs imported for invite

--- src/routes/auth/invite/+page.svelte
+++ src/routes/auth/invite/+page.svelte
@@ -1,34 +1,15 @@
 <script lang="ts">
     import { onMount } from 'svelte';
-	import Typography from '../../../components/typography.svelte';
-	import Card from '../../../components/card.svelte';
-	import Toggle from 'svelte-toggle';
-	import Meta from '../../../components/meta.svelte';
-	import Toast from '../../../components/toast.svelte';
-	import type { AccessibilitySettings } from '../../../types/accessibility';
 	import type { User } from '../../../types/account';
-	import { Check, WarningFilled } from '../../../components/icons';
 
     let success: boolean = false;
 	let failure: boolean = false;
 	let user: User;
 	let token: string;
-	let accessibility: AccessibilitySettings = {
-		sounds: true,
-		captions: false,
-		motion: true,
-		highContrast: false,
-		zoom: 'x1',
-		invertColors: false,
-		grayscale: false,
-		autoplay: true
-	};
+	
     let inviteLink: String;
     onMount(async () => {
 		user = JSON.parse(localStorage.getItem('huelet:auth:user') as string) as User;
-		accessibility = JSON.parse(
-			localStorage.getItem('huelet:auth:accessibility') as string
-		) as AccessibilitySettings;
 		token = localStorage.getItem('huelet:auth:token') as string;
 
 		if (!user || !token) return;
