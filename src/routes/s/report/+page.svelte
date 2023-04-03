7b896c5b1dfa0bdb43f59cf3d0de5d531be172a0
Daniel Kravec <daniel@kravec.com>
2023-04-03 19:10:15 -0400

    fixed link to url

--- src/routes/s/report/+page.svelte
+++ src/routes/s/report/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     // @ts-ignore
-    import api_url from '../../scripts/test.ts';
+    import api_url from '../../../env.ts';
 
 </script>
 <h1>You are very important to us. Please stand by with the problems...</h1>
