c7dd1b56201bb51c76b4f37e1c0f5a18bdd974d1
Daniel Kravec <daniel@kravec.com>
2023-04-03 19:07:06 -0400

    testing new api url

---
+++ src/env.ts
@@ -0,0 +1,4 @@
+
+let api_url:String = "https://api-production-3cf9.up.railway.app";
+
+export default api_url;
--- src/routes/s/report/+page.svelte
+++ src/routes/s/report/+page.svelte
@@ -1,1 +1,7 @@
+<script lang="ts">
+    // @ts-ignore
+    import api_url from '../../scripts/test.ts';
+
+</script>
 <h1>You are very important to us. Please stand by with the problems...</h1>
+<p>{api_url}</p>
