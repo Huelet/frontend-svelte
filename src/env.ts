2d36c0c1a2c9092b4abc998dc96c638d292de1b4
Daniel Kravec <daniel@kravec.com>
2023-04-03 19:29:48 -0400

    plz work

--- src/env.ts
+++ src/env.ts
@@ -2,4 +2,5 @@
 // make sure not to add an "/" at the end of the url
 let api_url:String = "https://api-production-3cf9.up.railway.app";
 let main_url:String = "https://huelet.net";
-export default {api_url, main_url};
+
+export {api_url, main_url};
