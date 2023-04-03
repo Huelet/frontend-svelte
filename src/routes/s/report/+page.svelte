f89994a0d91848c6652da26b6d5967442267172f
Daniel Kravec <daniel@kravec.com>
2023-04-03 19:25:37 -0400

    testing yet again

--- src/routes/s/report/+page.svelte
+++ src/routes/s/report/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
     // @ts-ignore
-    import api_url from '../../../env';
-    import main_url from '../../../env';
+    import {api_url, main_url} from '../../../env';
 </script>
 <h1>You are very important to us. Please stand by with the problems...</h1>
 <p>api {api_url}</p>
