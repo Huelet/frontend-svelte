4b90ef01ede177616f65ea6478403777509f2bef
Sam <103699066+Samthibau@users.noreply.github.com>
2023-04-13 17:22:00 -0500

    Update +page.svelte

--- src/routes/s/report/+page.svelte
+++ src/routes/s/report/+page.svelte
@@ -1,1 +1,31 @@
-<h1>You are very important to us. Please stand by with the problems...</h1>
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import Card from '../../../../components/card.svelte';
+	import Meta from '../../../../components/meta.svelte';
+	import type { AccessibilitySettings } from '../../../../types/accessibility';
+	import type { User } from '../../../../types/account';
+	let success: boolean = false;
+	let failure: boolean = false;
+	let user: User;
+	let token: string;
+	let accessibility: AccessibilitySettings = {
+		sounds: true,
+		captions: false,
+		motion: true,
+		highContrast: false,
+		zoom: 'x1',
+		invertColors: false,
+		grayscale: false,
+		autoplay: true
+	};
+	onMount(async () => {
+		user = JSON.parse(localStorage.getItem('huelet:auth:user') as string) as User;
+		accessibility = JSON.parse(
+			localStorage.getItem('huelet:auth:accessibility') as string
+		) as AccessibilitySettings;
+		token = localStorage.getItem('huelet:auth:token') as string;
+		if (!user || !token) return;
+	});
+</script>
+
+<Card full={true} title="This page is temporarily unavailable." sub="Currently under construction."/>
