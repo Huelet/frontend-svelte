59e78e7ae332c0acf140aba8331347463114cc36
Sam <103699066+Samthibau@users.noreply.github.com>
2023-04-17 21:10:11 -0500

    Forgot a dash 💀

--- src/env.ts
+++ src/env.ts
@@ -1,6 +1,6 @@
 // change url here, dont forget to change when needed
 // make sure not to add an "/" at the end of the url
-let api_url:String = "https:/api.huelet.wip.la";
+let api_url:String = "https://api.huelet.wip.la";
 let main_url:String = "https://huelet.wip.la";
 
 export {api_url, main_url};
