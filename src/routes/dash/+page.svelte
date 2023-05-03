7e97d4df6b23a5c096b5ebaf461bcc3f5d6ae088
Sam <103699066+Samthibau@users.noreply.github.com>
2023-05-03 17:33:27 -0500

    Dash welcom page UI update

--- src/routes/dash/+page.svelte
+++ src/routes/dash/+page.svelte
@@ -1,14 +1,47 @@
 <script lang="ts">
- // redirect to /dash/profile/me
+  // redirect to /dash/profile/me
 </script>
 
-<div>
-<h1>Creator dashboard</h1>
+<style>
+  /* Center the content of the div */
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    font-family: 'Red Hat Text', sans-serif;
+  }
 
-    <h2>
-        Welcome to the creator dashboard!
-    </h2>
-        press here (this will do nothing)
+  h1, h2 {
+    text-align: center;
+    font-weight: bold;
+  }
+
+  /* Style the button */
+  button {
+    background-color: #4CAF50;
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+  }
+</style>
+
+<div>
+  <h1 style="font-size: 36px"> <b>Creator dashboard</b></h1>
+  <h2>Welcome to the creator dashboard!</h2>
+  <button>Press Here (This will do nothing)</button>
 </div>
 
-export default IndexPage;
+<script>
+  // Export the component
+  export default {
+    name: "IndexPage",
+  };
+</script>
