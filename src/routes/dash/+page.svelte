b2c5fb5e284e248fa27d9ca943dfbd04318e99db
Sam <103699066+Samthibau@users.noreply.github.com>
2023-05-03 17:36:07 -0500

    Update +page.svelte Try 3 lol

--- src/routes/dash/+page.svelte
+++ src/routes/dash/+page.svelte
@@ -1,36 +1,6 @@
 <script lang="ts">
   // redirect to /dash/profile/me
 </script>
-
-<style>
-
-  div {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    height: 100vh;
-    font-family: 'Red Hat Text', sans-serif;
-  }
-  h1, h2 {
-    text-align: center;
-    font-weight: bold;
-  }
-
-  button {
-    background-color: #4CAF50;
-    border: none;
-    color: white;
-    padding: 15px 32px;
-    text-align: center;
-    text-decoration: none;
-    display: inline-block;
-    font-size: 16px;
-    margin: 4px 2px;
-    cursor: pointer;
-  }
-</style>
-
 <div>
   <h1 style="font-size: 36px"> <b>Creator dashboard</b></h1>
   <h2>Welcome to the creator dashboard!</h2>
