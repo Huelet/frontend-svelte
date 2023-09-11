18d31bdd36a6de030e4738931248677ce1ddb1e6
Sam <103699066+Samthibau@users.noreply.github.com>
2023-09-11 01:33:07 -0500

    Update env.ts

--- src/env.ts
+++ src/env.ts
@@ -1,6 +1,6 @@
 // change url here, dont forget to change when needed
 // make sure not to add an "/" at the end of the url
-let api_url:String = "https://api.huelet.wip.la";
-let main_url:String = "https://huelet.wip.la";
+let api_url:String = "api-production-7219.up.railway.app";
+let main_url:String = "https://socialmain-production-7e89.up.railway.app/";
 
 export {api_url, main_url};
