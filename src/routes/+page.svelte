1e4acf704dff22bbb6c17a0d50fa306d657ada52
Sam <103699066+Samthibau@users.noreply.github.com>
2023-09-12 01:45:11 -0500

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,123 +15,67 @@
 <Meta />
 
 <main>
-  <div class="hero">
-    <div class="logo-container">
-      <span on:contextmenu={(event) => { event.preventDefault(); }}>
-        <Logo dimensions={128} />
-      </span>
+  <div class="container-box">
+    <div class="hero">
+      <div class="logo-container">
+        <span on:contextmenu={(event) => { event.preventDefault(); }}>
+          <Logo dimensions={128} />
+        </span>
+      </div>
+      <div class="text-container">
+        <Typography weight={800} fontSize={2} class="center-text"> Welcome to Shards Social! </Typography>
+      </div>
+      <div class="button-container">
+        <a href="https://dsc.gg/huelet">
+          <div class="button primary rounded">Alpha tester Sign up</div>
+        </a>
+        <a href="/auth/in">
+          <div class="button secondary rounded">I already have an account</div>
+        </a>
+      </div>
     </div>
-    <div class="text-container">
-      <Typography weight={800} fontSize={2} class="center-text"> Welcome to Shards Social! </Typography>
-    </div>
-    <div class="button-container">
-      <a href="https://dsc.gg/huelet">
-        <div class="button primary rounded">Alpha tester Sign up</div>
-      </a>
-      <a href="/auth/in">
-        <div class="button secondary rounded">I already have an account</div>
-      </a>
-    </div>
-  </div>
-<footer>
-    <div class="footer-content">
-      <p>&copy; 2023 Shards Social</p>
-      <ul>
-        <li><a href="/about">About Us</a></li>
-        <li><a href="/contact">Contact Us</a></li>
-        <li><a href="/privacy">Privacy Policy</a></li>
-      </ul>
-    </div>
-  </footer>
+  </div> <!-- Closing the container-box div -->
 </main>
+
+<footer>
+  <div class="footer-content">
+    <p>&copy; 2023 Shards Social</p>
+    <ul>
+      <li><a href="/about">About Us</a></li>
+      <li><a href="/contact">Contact Us</a></li>
+      <li><a href="/privacy">Privacy Policy</a></li>
+    </ul>
+  </div>
+</footer>
+
+</main>
+
+
+
 <style>
-  body {
-    display: flex;
-    flex-direction: column;
-    min-height: 100vh; /* Ensure the body takes up at least the full viewport height */
-    margin: 0; /* Remove default margin */
-  }
+ /* Add this CSS to style the container box */
+.container-box {
+  border-radius: 8px;
+  padding: 20px; /* You can adjust the padding as needed */
+  width: 100%;
+  max-width: 536px; /* Setting a maximum width */
+  margin: 0 auto; /* Center the box horizontally */
+}
 
-  main {
-    flex-grow: 1; /* Allow the main content to grow and fill available space */
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
+/* Position the "Welcome to Shards Social!" text at the top */
+.center-text {
+  text-align: center;
+}
 
-  footer {
-    background-color: #181718;
-    padding: 1rem;
-    text-align: center;
-    width: 100%;
-  }
+/* Position the footer as specified */
+footer {
+  position: absolute;
+  left: 697px;
+  top: 1114px;
+  width: 536px;
+  height: 43px;
+}
 
-  .footer-content {
-    max-width: 1200px;
-    margin: 0 auto;
-    color: #333;
-  }
-
-  main {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-    width: 100vw;
-  }
-
-  a {
-    text-decoration: none;
-  }
-
-  a:visited {
-    color: var(--text-primary-dark);
-  }
-
-  .hero {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding: 1rem;
-  }
-
-  .logo-container {
-    margin-bottom: 1rem;
-  }
-
-  .button-container {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
-
-  .button {
-    padding: 0.5rem 1rem;
-    border-radius: 25px;
-    margin: 0.5rem;
-    cursor: pointer;
-    text-align: center;
-  }
-
-  .primary {
-    background-color: #00bfff; /* Light blue color for primary button */
-    color: #fff;
-  }
-
-  .secondary {
-    background-color: #00bfff; /* Light blue color for secondary button */
-    color: #fff;
-  }
-
-  .center-text {
-    text-align: center; /* Center-align text */
-  }
-
-  @media (min-width: 768px) {
-    .hero {
-      padding: 2rem;
-    }
-  }
 </style>
 
 
