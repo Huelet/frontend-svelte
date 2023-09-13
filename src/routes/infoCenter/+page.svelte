2be8826105e60e7d6ade5a6c58c9a6445e732438
Sam <103699066+Samthibau@users.noreply.github.com>
2023-09-12 22:34:58 -0500

    add infocenter again

---
+++ src/routes/infoCenter/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+    import { navigate } from "@sapper/app";
+  
+    // Function to navigate to the "/infocenter" route
+    function goToInfoCenter() {
+      navigate("/infocenter");
+    }
+  </script>
+  
+  <main>
+    <h1>Shards Social Information Center</h1>
+    <button on:click={goToInfoCenter}>Button 1</button>
+    <button on:click={goToInfoCenter}>Button 2</button>
+    <button on:click={goToInfoCenter}>Button 3</button>
+    <button on:click={goToInfoCenter}>Button 4</button>
+    <button on:click={goToInfoCenter}>Button 5</button>
+  </main>
+  
+  <style>
+    /* You can add CSS styles here */
+  </style>
+  
