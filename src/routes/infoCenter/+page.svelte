02572fd30e8db639a96f9add8ef3792a990b32d2
Sam <103699066+Samthibau@users.noreply.github.com>
2023-09-12 22:42:55 -0500

    Update +page.svelte

--- src/routes/infoCenter/+page.svelte
+++ src/routes/infoCenter/+page.svelte
@@ -1,18 +1,17 @@
-<script>
-  // Import the `link` function from SvelteKit
-  import { link } from '$app/navigation';
-</script>
 
-<main>
 
-    <h1>Shards Social Information Center</h1>
-  <button on:click={() => link('/infocenter')}>Button 1</button>
-  <button on:click={() => link('/infocenter')}>Button 2</button>
-  <button on:click={() => link('/infocenter')}>Button 3</button>
-  <button on:click={() => link('/infocenter')}>Button 4</button>
-  <button on:click={() => link('/infocenter')}>Button 5</button>
-</main>
+<div class="button-container">
+  <button>Button 1</button>
+  <button>Button 2</button>
+  <button>Button 3</button>
+  <button>Button 4</button>
+  <button>Button 5</button>
+</div>
 
 <style>
-  /* You can add CSS styles here */
+  .button-container {
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+  }
 </style>
