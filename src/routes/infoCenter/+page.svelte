0a72e9cd325c65b4becf871351ce37ff5b476477
Sam <103699066+Samthibau@users.noreply.github.com>
2023-09-12 22:38:12 -0500

    Update +page.svelte

--- src/routes/infoCenter/+page.svelte
+++ src/routes/infoCenter/+page.svelte
@@ -1,22 +1,18 @@
 <script>
-    import { navigate } from "@sapper/app";
-  
-    // Function to navigate to the "/infocenter" route
-    function goToInfoCenter() {
-      navigate("/infocenter");
-    }
-  </script>
-  
-  <main>
+  // Import the `link` function from SvelteKit
+  import { link } from '$app/navigation';
+</script>
+
+<main>
+
     <h1>Shards Social Information Center</h1>
-    <button on:click={goToInfoCenter}>Button 1</button>
-    <button on:click={goToInfoCenter}>Button 2</button>
-    <button on:click={goToInfoCenter}>Button 3</button>
-    <button on:click={goToInfoCenter}>Button 4</button>
-    <button on:click={goToInfoCenter}>Button 5</button>
-  </main>
-  
-  <style>
-    /* You can add CSS styles here */
-  </style>
-  
+  <button on:click={() => link('/infocenter')}>Button 1</button>
+  <button on:click={() => link('/infocenter')}>Button 2</button>
+  <button on:click={() => link('/infocenter')}>Button 3</button>
+  <button on:click={() => link('/infocenter')}>Button 4</button>
+  <button on:click={() => link('/infocenter')}>Button 5</button>
+</main>
+
+<style>
+  /* You can add CSS styles here */
+</style>
