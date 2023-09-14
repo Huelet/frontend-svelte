6f2813a3ee82b06528bd1c359fd936de38e63a8c
Sam <103699066+Samthibau@users.noreply.github.com>
2023-09-13 21:24:36 -0500

    Update +layout.svelte

--- src/routes/explore/+layout.svelte
+++ src/routes/explore/+layout.svelte
@@ -126,12 +126,12 @@
 
     .sidebar {
         position: fixed;
-        top: 0;
-        left: 0.5cm; /* Moved half a centimeter from the left edge */
+        top: 1in; /* Lowered by an inch */
+        right: 0.5cm; /* Moved 0.5cm away from the right edge */
         width: 55px;
-        height: 245px;
+        height: 245px; /* Set to 245px */
         background-color: #343333;
-        border-radius: 8px; /* Apply the same radius to all corners */
+        border-radius: 8px; /* All corners with radius 8 */
         display: flex;
         flex-direction: column;
         align-items: center;
@@ -146,7 +146,7 @@
 
     .sidebar-button {
         background-color: #646464;
-        border-radius: 8px; /* Apply the same radius to all corners */
+        border-radius: 8px; /* All corners with radius 8 */
         padding: 0.25em 0.5em; /* Reduced button size */
         color: white;
         border: none;
