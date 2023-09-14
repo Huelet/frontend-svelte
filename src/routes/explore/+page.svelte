e27ee5d85801681741fc3da67032d8e519a7da35
Sam <103699066+Samthibau@users.noreply.github.com>
2023-09-14 13:46:37 -0500

    Update +page.svelte

--- src/routes/explore/+page.svelte
+++ src/routes/explore/+page.svelte
@@ -1,61 +1,60 @@
 <script lang="ts">
-	import type { PageData } from './$types';
-	import VideoCard from '../../components/video-card.svelte';
-	import Loading from '../../components/loading.svelte';
-	import Typography from '../../components/typography.svelte';
-	import Meta from '../../components/meta.svelte';
-	import '@fontsource/red-hat-display/300.css';
+  import type { PageData, Video } from './$types';
+  import VideoCard from '../../components/video-card.svelte';
+  import Loading from '../../components/loading.svelte';
+  import Typography from '../../components/typography.svelte';
+  import Meta from '../../components/meta.svelte';
+  import '@fontsource/red-hat-display/300.css';
 
-	export let data: PageData;
+  export let data: PageData;
 
-	let videoList: any[] | undefined = data.videoList;
+  let videoList: Video[] | undefined = data.videoList;
 </script>
 
 <Meta
-	title="Explore | Shards"
-	description=" Shards a better social media Experience"
+  title="Explore | Shards"
+  description="Shards: A Better Social Media Experience"
 />
 
 <main>
-	<div class="fyp-container">
-		<div class="fyp-videos">
-			<div class="fyp-videos-grid">
-				{#if videoList}
-					{#each videoList as video}
-						<VideoCard {video} />
-					{/each}
-				{:else}
-					<Loading dimensions={256} />
-					<Typography size="xl">Loading Fyp</Typography>
-				{/if}
-			</div>
-		</div>
-	</div>
+  <div class="explore-container">
+    <div class="explore-videos">
+      <div class="explore-videos-grid">
+        {#if videoList}
+          {#each videoList as video}
+            <VideoCard {video} />
+          {/each}
+        {:else}
+          <Loading dimensions={256} />
+          <Typography size="xl">Loading Explore</Typography>
+        {/if}
+      </div>
+    </div>
+  </div>
 </main>
 
 <style>
-	main {
-		height: 96vh;
-		width: 96vw;
-	}
+  main {
+    height: 96vh;
+    width: 96vw;
+  }
 
-	.fyp-container {
-		height: 100%;
-		width: 100%;
-	}
+  .explore-container {
+    height: 100%;
+    width: 100%;
+  }
 
-	.fyp-videos {
-		display: grid;
+  .explore-videos {
+    display: grid;
+    padding: 1em;
+    height: 100%;
+    width: 100%;
+  }
 
-		padding: 1em;
-		height: 100%;
-		width: 100%;
-	}
-
-	.fyp-videos > .fyp-videos-grid {
-		display: grid;
-		place-items: center;
-		grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
-		grid-gap: 1em;
-	}
+  .explore-videos > .explore-videos-grid {
+    display: grid;
+    place-items: center;
+    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+    grid-gap: 1em;
+  }
 </style>
