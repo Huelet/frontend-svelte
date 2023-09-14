b9a2bead3f4a9b1193298a619969919f40efc39d
Sam <103699066+Samthibau@users.noreply.github.com>
2023-09-13 21:49:31 -0500

    Update +layout.svelte

--- src/routes/explore/+layout.svelte
+++ src/routes/explore/+layout.svelte
@@ -4,52 +4,8 @@
 	import Typography from '../../components/typography.svelte';
 	import { onMount } from 'svelte';
 	import { News, Refresh, Stocks } from '../../components/icons';
-	import {api_url} from '../../env';
-
-	let timeOfDay: string;
-	let weather: string;
-	let locationData: any;
-	let user: any;
-	let following: any[] = [];
-
-	const now = new Date();
-	const hour = now.getHours();
-
-	if (hour < 12) timeOfDay = 'morning';
-	else if (hour < 18) timeOfDay = 'afternoon';
-	else timeOfDay = 'evening';
-
-	onMount(async () => {
-		const userId = localStorage.getItem('userId');
-		localStorage.getItem(`user-${userId}`)
-			? (user = JSON.parse(localStorage.getItem(`user-${userId}`) as string))
-			: (user = null);
-
-		user?.following.forEach((user: any) => {
-			fetch(`${api_url}/auth/user?username=${user}`)
-				.then((res) => res.json())
-				.then((data) => {
-					following.push(data.data);
-				});
-		});
-	});
-
-	fetch('https://ipapi.co/json/')
-		.then((response) => response.json())
-		.then((data) => {
-			locationData = data;
-			// i know this includes my key but i do not give a hint of a reason to care
-			fetch(
-				`https://api.weatherapi.com/v1/current.json?key=1e9c6dd478af4f6fa8205430222106&q=${locationData.latitude},${locationData.longitude}&aqi=no`
-			)
-				.then((response) => response.json())
-				.then((data) => {
-					weather = data.current.condition.text;
-				});
-		});
+	import { api_url } from '../../env';
 </script>
-
-
 
 <main>
     <div class="sidebar">
@@ -104,8 +60,6 @@
         </div>
     </div>
 </main>
-
-
 
 
 <style>
